64f5f98a413f0a10f2a6cf22f5951020fae5ac10
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2022-12-25 21:04:46 -0500

    Add score variables and scores to playRound()

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const scissorsButton = document.querySelector('#scissors');
 const result = document.querySelector('.result');
 const score = document.querySelector('.score');
 
+// Define player and computer (COM) scores
+let playerScore = 0;
+let computerScore = 0;
 
 /**
  * This function generates a choice of rock, paper, scissors for the computer (COM).
@@ -29,28 +32,43 @@ let playRound = (playerSelection, computerChoice) => {
     switch(playerSelection.toLowerCase())
     {
         case 'rock':
-            if (computerChoice === 'rock')
+            if (computerChoice === 'rock'){
                 result.textContent = "It's a tie! Rock - Rock";
-            else if (computerChoice === 'paper')
+            }
+            else if (computerChoice === 'paper'){
+                computerScore++;
                 result.textContent = "You lose! Paper beats Rock!";
-            else if (computerChoice === 'scissors')
+            }
+            else if (computerChoice === 'scissors'){
+                playerScore++;
                 result.textContent = "You win! Rock beats Scissors!";
+            }
             break;
         case 'paper':
-            if (computerChoice === 'rock')
+            if (computerChoice === 'rock'){
+                playerScore++;
                 result.textContent = 'You win! Paper beats Rock!';
-            else if (computerChoice === 'paper')
+            }
+            else if (computerChoice === 'paper'){
                 result.textContent = "It's a tie! Paper - Paper!";
-            else if (computerChoice === 'scissors')
+            }
+            else if (computerChoice === 'scissors'){
+                computerScore++;
                 result.textContent = 'You lose! Scissors beats Paper!';
+            }
             break;
         case 'scissors':
-            if (computerChoice === 'rock')
-                result.textContent = 'You lose! Rock beats Scissors!'
-            else if (computerChoice === 'paper')
-                result.textContent = 'You win! Scissors beats Paper'
-            else if (computerChoice === 'scissors')
+            if (computerChoice === 'rock'){
+                computerScore++;
+                result.textContent = 'You lose! Rock beats Scissors!';
+            }
+            else if (computerChoice === 'paper'){
+                playerScore++;
+                result.textContent = 'You win! Scissors beats Paper';
+            }
+            else if (computerChoice === 'scissors'){
                 result.textContent = "It's a tie! Scissors - Scissors!"
+            }
             break;
         default:
             result.textContent = 'Invalid player choice. Enter rock, paper, or scissors.';
